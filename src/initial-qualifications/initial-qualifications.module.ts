cd3aca95688e1514ec577f4902eddbe63c8575dc
Lucas Previato <lucaspreviato@Live.com>
2023-05-17 22:10:42 -0300

    feat(initialqualification): initial qualification module

--- src/initial-qualifications/initial-qualifications.module.ts
+++ src/initial-qualifications/initial-qualifications.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { InitialQualificationsController } from './initial-qualifications.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  controllers: [InitialQualificationsController]
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'INITIAL_QUALIFICATIONS_SERVICE_NAME',
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50052',
+          package: 'INITIAL_QUALIFICATIONS_PACKAGE_NAME',
+          protoPath:
+            'node_modules/grpc-proto/proto/initial-qualifications.proto',
+        },
+      },
+    ]),
+  ],
+  controllers: [InitialQualificationsController],
 })
 export class InitialQualificationsModule {}
